99be27bdc24b2ea578ce8b06b242375e3b7ee03f
Salim Mujawar <salim685@gmail.com>
2019-06-27 17:02:12 +0400

    Added button onclick listener

--- app/src/main/java/com/example/fitness/MainActivity.java
+++ app/src/main/java/com/example/fitness/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.fitness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXERCISE_WEIGHTS = "Weight";
+    public static final String EXERCISE_YOGA = "Yoga";
+    public static final String EXERCISE_CARDIO = "Cardio";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout btnWeight = (RelativeLayout) findViewById(R.id.btnWeight);
+        RelativeLayout btnYoga   = (RelativeLayout) findViewById(R.id.btnYoga);
+        RelativeLayout btnCardio = (RelativeLayout) findViewById(R.id.btnCardio);
+
+        btnWeight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(EXERCISE_WEIGHTS);
+            }
+        });
+
+        btnYoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(EXERCISE_YOGA);
+            }
+        });
+
+        btnCardio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDetailActivity(EXERCISE_CARDIO);
+            }
+        });
+
+    }
+
+    private void loadDetailActivity(String exerciseTitle) {
+
     }
 }
